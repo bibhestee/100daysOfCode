103c78b6a9919a0f71a5da7b1f93da64e56cae07
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-02-25 07:52:45 +0100

    add two new handlers to handle snippetview and snippet create, also restrict home url to '/' only

--- GoLang/snippetbox/main.go
+++ GoLang/snippetbox/main.go
@@ -5,17 +5,35 @@ import (
   "net/http"
 )
 
-// Define a home handler function which writes a bite slice containing
-// "Hello world from Snippetbox" as the response body
+// Define a home handler function which writes a byte slice containing
+// Display the home page using "Hello world from Snippetbox" as the response body
 func home(res http.ResponseWriter, req *http.Request) {
+  // Check if the current URL path exactly matches "/"
+  if req.URL.Path != "/" {
+    http.NotFound(res, req)
+    return
+  }
   res.Write([]byte("Hello world from Snippetbox"))
 }
 
+// Display a specific snippet
+func snippetView(res http.ResponseWriter, req *http.Request) {
+  res.Write([]byte("Display a specific snippet..."))
+}
+
+// Create a new snippet
+func snippetCreate(res http.ResponseWriter, req *http.Request) {
+  res.Write([]byte("Create a new snippet..."))
+}
+
+
 func main() {
   // Use http.NewServeMux() function to initialize a new servemux, then
-  // register the home function as the handler for the "/" URL pattern
+  // register the home function as the handler for the "/" URL pattern and the two new handlers
   mux := http.NewServeMux()
   mux.HandleFunc("/", home)
+  mux.HandleFunc("/snippet/view", snippetView)
+  mux.HandleFunc("/snippet/create", snippetCreate)
 
   // Use the http.ListenAndServe() function to start a new web server.
   // We pass in two parameters: TCP network address to listen on ( in this case ":4000")
