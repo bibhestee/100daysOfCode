a7fff63fed3850710fd5b71c68c96c9e000a21ed
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-02-27 15:03:22 +0100

    add logic to create new snippet and insert to database

--- GoLang/snippetbox/internal/models/snippets.go
+++ GoLang/snippetbox/internal/models/snippets.go
@@ -21,7 +21,20 @@ type SnippetModel struct {
 
 // Insert new record into database
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-  return 0, nil
+
+  stmt := `INSERT INTO snippets (title, content, created, expires) VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+  result, err := m.DB.Exec(stmt, title, content, expires)
+  if err != nil {
+    return 0, err
+  }
+
+  id, err := result.LastInsertId()
+  if err != nil {
+    return 0, nil
+  }
+
+  return int(id), nil
 }
 
 // Get specific snippet based on ID
