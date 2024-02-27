5e4a1c7fc3fe36c8a1bf25bea3eb22e019eecc31
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-02-27 14:20:22 +0100

    create a database connection pool

--- GoLang/snippetbox/cmd/web/main.go
+++ GoLang/snippetbox/cmd/web/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+  "database/sql"
   "flag"
   "log"
   "net/http"
   "os"
+  _ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -16,6 +18,7 @@ type application struct {
 func main() {
 
   addr := flag.String("addr", ":4000", "HTTP network address")
+  dsn := flag.String("dsn", "web:web@/snippetbox?parseTime=true", "MySQL data source name")
 
   flag.Parse()
 
@@ -27,6 +30,14 @@ func main() {
     infoLog: infoLog,
   }
 
+  // Connect db
+  db, err := openDB(*dsn)
+  if err != nil {
+    errorLog.Fatal(err)
+  }
+
+  // Close db connection
+  defer db.Close()
 
   srv := &http.Server{
     Addr: *addr,
@@ -35,6 +46,19 @@ func main() {
   }
 
   infoLog.Printf("Starting server on %s", *addr)
-  err := srv.ListenAndServe()
+  err = srv.ListenAndServe()
   errorLog.Fatal(err)
+}
+
+// openDB() function wraps sql.Open() and returns a sql.DB connection pool for a given dns
+func openDB(dsn string) (*sql.DB, error) {
+  db, err := sql.Open("mysql", dsn)
+  if err != nil {
+     return nil, err
+  }
+  // Test the connection
+  if err = db.Ping(); err != nil {
+    return nil, err
+  }
+  return db, nil
 }
