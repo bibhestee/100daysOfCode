56782daaaca1e491a1664e7be11efb8b0deab7fe
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-02-28 22:47:36 +0100

    update the Get method to retrieve snippet from database and return a single row

--- GoLang/snippetbox/internal/models/snippets.go
+++ GoLang/snippetbox/internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
   "database/sql"
+  "errors"
   "time"
 )
 
@@ -39,7 +40,21 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 
 // Get specific snippet based on ID
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
-  return nil, nil
+  stmt := `SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+  row := m.DB.QueryRow(stmt, id)
+
+  s := &Snippet{}
+
+  err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+  if err != nil {
+    if errors.Is(err, sql.ErrNoRows) {
+      return nil, ErrNoRecord
+    } else {
+      return nil, err
+    }
+  }
+  return s, nil
 }
 
 // This will return 10 most recently created snippet
