d64485efa19ee62d031e25cb5d567f896ea9226c
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-02-29 20:59:02 +0100

    update SnippetModel Latest method to get the 10 most recent snippets

--- GoLang/snippetbox/internal/models/snippets.go
+++ GoLang/snippetbox/internal/models/snippets.go
@@ -59,5 +59,36 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 
 // This will return 10 most recently created snippet
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
-  return nil, nil
+
+  stmt := `SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+
+  rows, err := m.DB.Query(stmt)
+
+  if err != nil {
+    return nil, err
+  }
+
+  defer rows.Close()
+
+  // Initialize an empty slice to hold the snippet struct
+  snippets := []*Snippet{}
+
+  for rows.Next() {
+    s := &Snippet{}
+    // Use rows.Scan to copy the values for each field in the row to the new Snippet object
+    err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+    if err != nil {
+      return nil, err
+    }
+
+    snippets = append(snippets, s)
+  }
+
+  // When the rows.Next() loop has finished we call rows.Err() to retrieve any
+  // error that was encountered during the iteration.
+  if err = rows.Err(); err != nil {
+    return nil, err
+  }
+
+  return snippets, nil
 }
