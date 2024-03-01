e418e3cf916584903fda760b4b34557ae02b7287
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-01 08:44:34 +0100

    add new field Snippets to templateData to hold list of snippets

--- GoLang/snippetbox/cmd/web/templates.go
+++ GoLang/snippetbox/cmd/web/templates.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-  "github.com/bibhestee/100daysOfCode/GoLang/snippetbox"
+  "github.com/bibhestee/100daysOfCode/GoLang/snippetbox/internal/models"
 )
 
 
 type templateData struct {
   Snippet *models.Snippet
+  Snippets []*models.Snippet
 }
