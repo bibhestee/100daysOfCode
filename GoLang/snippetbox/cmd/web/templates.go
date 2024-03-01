610325f38210b79b50db9d4c7b4e52bbf55b4c93
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-01 09:39:28 +0100

    implement a func to automatically parse the template files

--- GoLang/snippetbox/cmd/web/templates.go
+++ GoLang/snippetbox/cmd/web/templates.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+  "html/template"
+  "path/filepath"
   "github.com/bibhestee/100daysOfCode/GoLang/snippetbox/internal/models"
 )
 
@@ -8,4 +10,39 @@ import (
 type templateData struct {
   Snippet *models.Snippet
   Snippets []*models.Snippet
+}
+
+
+func newTemplateCache() (map[string]*template.Template, error) {
+  cache := map[string]*template.Template{}
+
+  pages, err := filepath.Glob("./ui/html/pages/*.html")
+  if err != nil {
+    return nil, err
+  }
+
+  for _, page := range pages {
+    // Extract filename
+    name := filepath.Base(page)
+
+    ts, err := template.ParseFiles("./ui/html/base.html")
+    if err != nil {
+      return nil, err
+    }
+
+    ts, err = ts.ParseGlob("./ui/html/partials/*.html")
+    if err != nil {
+      return nil, err
+    }
+
+    ts, err = ts.ParseFiles(page)
+    if err != nil {
+      return nil, err
+    }
+
+    cache[name] = ts
+  }
+
+  return cache, nil
+
 }
