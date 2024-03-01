6958706084debb87070186d865e22a4d64788702
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-01 09:40:41 +0100

    implement a render method on application to render different views based on argument passed

--- GoLang/snippetbox/cmd/web/helpers.go
+++ GoLang/snippetbox/cmd/web/helpers.go
@@ -22,3 +22,21 @@ func (app *application) clientError(res http.ResponseWriter, status int) {
 func (app *application) notFound(res http.ResponseWriter) {
   app.clientError(res, http.StatusNotFound)
 }
+
+
+func (app *application) render(res http.ResponseWriter, status int, page string, data *templateData) {
+  ts, ok := app.templateCache[page]
+  if !ok {
+    err := fmt.Errorf("the template %s does not exist", page)
+    app.serverError(res, err)
+    return
+  }
+
+  res.WriteHeader(status)
+
+  err := ts.ExecuteTemplate(res, "base", data)
+  if err != nil {
+    app.serverError(res, err)
+  }
+
+}
