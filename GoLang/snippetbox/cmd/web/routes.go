f3f743e7445cd7ac0faca0e044c2c465f159bd1e
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-01 17:19:43 +0100

    update the router to use the new logRequest middleware

--- GoLang/snippetbox/cmd/web/routes.go
+++ GoLang/snippetbox/cmd/web/routes.go
@@ -16,5 +16,5 @@ func (app *application) routes() http.Handler {
   mux.HandleFunc("/snippet/view", app.snippetView)
   mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-  return secureHeader(mux)
+  return app.logRequest(secureHeader(mux))
 }
