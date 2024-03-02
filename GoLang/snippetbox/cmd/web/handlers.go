13d4fd309f95f795e4a35184ce604e3fa0e17b67
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-02 18:52:30 +0100

    implement the new validator helper

--- GoLang/snippetbox/cmd/web/handlers.go
+++ GoLang/snippetbox/cmd/web/handlers.go
@@ -5,8 +5,7 @@ import (
   "fmt"
   "net/http"
   "strconv"
-  "strings"
-  "unicode/utf8"
+  "github.com/bibhestee/100daysOfCode/Golang/snippetbox/internal/validator"
   "github.com/bibhestee/100daysOfCode/GoLang/snippetbox/internal/models"
   "github.com/julienschmidt/httprouter"
 )
@@ -15,7 +14,7 @@ type snippetCreateForm struct {
   Title string
   Content string
   Expires int
-  FieldErrors map[string]string
+  validator.Validator
 }
 
 
@@ -89,27 +88,17 @@ func (app *application) snippetCreatePost(res http.ResponseWriter, req *http.Req
     Title: req.PostForm.Get("title"),
     Content: req.PostForm.Get("content"),
     Expires: expires,
-    FieldErrors: map[string]string{},
   }
 
   // Validate data
+  form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
 
-  if strings.TrimSpace(form.Title) == "" {
-    form.FieldErrors["title"] = "This field cannot be blank"
-  } else if utf8.RuneCountInString(form.Title) > 100 {
-    form.FieldErrors["title"] = "This field cannot be more than 100 characters long."
-  }
-
-  if strings.TrimSpace(form.Content) == "" {
-    form.FieldErrors["content"] = "This field cannot be blank"
-  }
-
-  if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-    form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
-  }
+  form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long.")
+  form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+  form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
 
-  if len(form.FieldErrors) > 0 {
+  if !form.Valid() {
     data := app.newTemplateData(req)
     data.Form = form
     app.render(res, http.StatusUnprocessableEntity, "create.html", data)
