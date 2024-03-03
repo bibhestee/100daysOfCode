f3bd2473fc55a486f8c5dfd636f639f7ebd1387d
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-03 22:53:13 +0100

    add new tls configs

--- GoLang/snippetbox/cmd/web/main.go
+++ GoLang/snippetbox/cmd/web/main.go
@@ -80,6 +80,9 @@ func main() {
     ErrorLog: errorLog,
     Handler: app.routes(),
     TLSConfig: tlsConfig,
+    IdleTimeout: time.Minute,
+    ReadTimeout: 5 * time.Second,
+    WriteTimeout: 10 * time.Second,
   }
 
   infoLog.Printf("Starting server on %s", *addr)
