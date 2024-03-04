3341bdfa11a29068d69ed1f03671607156765e2e
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-04 20:20:49 +0100

    update the Insert method to populate the database with new user data

--- GoLang/snippetbox/internal/models/users.go
+++ GoLang/snippetbox/internal/models/users.go
@@ -2,6 +2,10 @@ package models
 
 import (
   "database/sql"
+  "errors"
+  "strings"
+  "github.com/go-sql-driver/mysql"
+  "golang.org/x/crypto/bcrypt"
   "time"
 )
 
@@ -19,6 +23,24 @@ type UserModel struct {
 
 
 func (m *UserModel) Insert(name, email, password string) error {
+  hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+  if err != nil {
+    return err
+  }
+
+  stmt := `INSERT INTO users (name, email, hashed_password, created) VALUE(?, ?, ?, UTC_TIMESTAMP())`
+
+  _, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+  if err != nil {
+    var mySQLError *mysql.MySQLError
+
+    if errors.As(err, &mySQLError) {
+      if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message, "user_uc_email") {
+        return ErrDuplicateEmail
+      }
+    }
+    return err
+  }
   return nil
 }
 
