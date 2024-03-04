05e9cfe3dfbf7c7364259d7429e0e84cfb160f4d
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-04 20:20:03 +0100

    add business logic to userSignupPost method to handle user signup

--- GoLang/snippetbox/cmd/web/handlers.go
+++ GoLang/snippetbox/cmd/web/handlers.go
@@ -124,7 +124,43 @@ func (app *application) userSignup(res http.ResponseWriter, req *http.Request) {
 }
 
 func (app *application) userSignupPost(res http.ResponseWriter, req *http.Request) {
-  fmt.Fprintln(res, "Create a new user...")
+  var form userSignupForm
+
+  err := app.decodePostForm(req, &form)
+  if err != nil {
+    app.clientError(res, http.StatusBadRequest)
+    return
+  }
+
+  // Validate
+  form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
+  form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+  form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+  form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+  form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
+
+
+  if !form.Valid() {
+    data := app.newTemplateData(req)
+    data.Form = form
+    app.render(res, http.StatusUnprocessableEntity, "signup.html", data)
+  }
+  // Add form data to database
+  err = app.users.Insert(form.Name, form.Email, form.Password)
+  if err != nil {
+    if errors.Is(err, models.ErrDuplicateEmail) {
+      form.AddFieldError("email", "Email address is already in use")
+      data := app.newTemplateData(req)
+      data.Form = form
+      app.render(res, http.StatusUnprocessableEntity, "signup.html", data)
+    } else {
+      app.serverError(res, err)
+    }
+    return
+  }
+
+  app.sessionManager.Put(req.Context(), "flash", "Your signup was successful. Please log in.")
+  http.Redirect(res, req, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) userLogin(res http.ResponseWriter, req *http.Request) {
