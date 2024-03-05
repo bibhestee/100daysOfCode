094169cd12f31786e8e5bf504453cdcd6980ad2c
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-05 06:06:24 +0100

    add NonFieldErrors to validator field and implement methods to modify them

--- GoLang/snippetbox/internal/validator/validator.go
+++ GoLang/snippetbox/internal/validator/validator.go
@@ -9,12 +9,17 @@ import (
 var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 type Validator struct {
-  FieldErrors map[string]string
+  NonFieldErrors []string
+  FieldErrors    map[string]string
 }
 
 
 func (v *Validator) Valid() bool {
-  return len(v.FieldErrors) == 0
+  return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+  v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 func (v *Validator) AddFieldError(key, message string) {
