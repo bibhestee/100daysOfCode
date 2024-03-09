0ce77ac8d9e1fc6c57e128092acbc8a87a6a99a3
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-09 19:48:17 +0100

    add the authenticate method to the routes

--- GoLang/snippetbox/cmd/web/routes.go
+++ GoLang/snippetbox/cmd/web/routes.go
@@ -22,7 +22,7 @@ func (app *application) routes() http.Handler {
 
   router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-  dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
+  dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
   router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 
