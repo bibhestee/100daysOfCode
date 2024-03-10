2b7a7a8a83789a40edd0235d793aaf99539993f9
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-10 18:15:00 +0100

    implement the embed ui file in newTemplateCache

--- GoLang/snippetbox/cmd/web/templates.go
+++ GoLang/snippetbox/cmd/web/templates.go
@@ -2,8 +2,10 @@ package main
 
 import (
   "html/template"
+  "io/fs"
   "path/filepath"
   "github.com/bibhestee/100daysOfCode/GoLang/snippetbox/internal/models"
+  "github.com/bibhestee/100daysOfCode/GoLang/snippetbox/ui"
   "time"
 )
 
@@ -22,7 +24,7 @@ type templateData struct {
 func newTemplateCache() (map[string]*template.Template, error) {
   cache := map[string]*template.Template{}
 
-  pages, err := filepath.Glob("./ui/html/pages/*.html")
+  pages, err := fs.Glob(ui.Files, "html/pages/*.html")
   if err != nil {
     return nil, err
   }
@@ -31,17 +33,13 @@ func newTemplateCache() (map[string]*template.Template, error) {
     // Extract filename
     name := filepath.Base(page)
 
-    ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
-    if err != nil {
-      return nil, err
+    patterns := []string{
+      "html/base.html",
+      "html/partials/*.html",
+      page,
     }
 
-    ts, err = ts.ParseGlob("./ui/html/partials/*.html")
-    if err != nil {
-      return nil, err
-    }
-
-    ts, err = ts.ParseFiles(page)
+    ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
     if err != nil {
       return nil, err
     }
