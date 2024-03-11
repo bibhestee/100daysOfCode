0a4622ea4ff971e85fc4857ebcb13012a05342f3
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-11 08:57:33 +0100

    update the humanDate function to satisfy the test case

--- GoLang/snippetbox/cmd/web/templates.go
+++ GoLang/snippetbox/cmd/web/templates.go
@@ -53,7 +53,11 @@ func newTemplateCache() (map[string]*template.Template, error) {
 
 
 func humanDate(t time.Time) string {
-  return t.Format("02 Jan 2006 at 3:04pm")
+  if t.IsZero() {
+    return ""
+  }
+
+  return t.UTC().Format("02 Jan 2006 at 3:04pm")
 }
 
 
