9a66f3329b9e176fbd84ff24847ab6be4e5f415c
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-13 17:47:36 +0100

    add method: postForm for sending test post requests

--- GoLang/snippetbox/cmd/web/testutils_test.go
+++ GoLang/snippetbox/cmd/web/testutils_test.go
@@ -8,6 +8,7 @@ import (
   "net/http"
   "net/http/cookiejar"
   "net/http/httptest"
+  "net/url"
   "regexp"
   "testing"
   "time"
@@ -75,6 +76,21 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
   return rs.StatusCode, rs.Header, string(body)
 }
 
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+  rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  defer rs.Body.Close()
+  body, err := io.ReadAll(rs.Body)
+  if err != nil {
+    t.Fatal()
+  }
+  bytes.TrimSpace(body)
+
+  return rs.StatusCode, rs.Header,string(body)
+}
 
 var csrfTokenRX = regexp.MustCompile(`<input type="hidden" name="csrf_token" value="(.+)">`)
 
