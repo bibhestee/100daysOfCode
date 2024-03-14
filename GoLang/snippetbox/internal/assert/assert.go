8455cd1b836f813fd1ba94906746a401870f0c16
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-14 21:25:23 +0100

    add test helper: create helper for checking if error is nil

--- GoLang/snippetbox/internal/assert/assert.go
+++ GoLang/snippetbox/internal/assert/assert.go
@@ -20,3 +20,12 @@ func StringContains(t *testing.T, actual, expectedSubstring string) {
     t.Errorf("got: %q; expected to contain: %q", actual, expectedSubstring)
   }
 }
+
+
+func NilError(t *testing.T, actual error) {
+  t.Helper()
+
+  if actual != nil {
+    t.Errorf("got: %v; expected: nil", actual)
+  }
+}
