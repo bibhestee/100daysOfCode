1293526e8c1049df3073393bbe4c6514ee79ef3f
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-14 21:26:41 +0100

    add user test: test the ModelExists method using integration testing

---
+++ GoLang/snippetbox/internal/models/users_test.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+  "testing"
+  "github.com/bibhestee/100daysOfCode/GoLang/snippetbox/internal/assert"
+)
+
+func TestUserModelExists(t *testing.T) {
+  tests := []struct {
+    name string
+    userID int
+    want bool
+    }{
+      {  name: "Valid ID",  userID: 1,  want: true,  },
+      {  name: "Zero ID",  userID: 0,  want: false,  },
+      {  name: "Non-existent ID",  userID: 2,  want: false,  },
+    }
+
+  for _, tt := range tests {
+    t.Run(tt.name, func(t *testing.T) {
+
+      db := newTestDB(t)
+
+      m := UserModel{db}
+
+      exists, err := m.Exists(tt.userID)
+
+      assert.Equal(t, exists, tt.want)
+      assert.NilError(t, err)
+    })
+  }
+}
+
