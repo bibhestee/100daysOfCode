f397ff7eabc3088ed81dfd8c50cd5bdd84511e84
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-20 20:04:07 +0100

    enable recovery mode to deliver realtime messages when connection is re-established

--- socket-chat-app/server.js
+++ socket-chat-app/server.js
@@ -6,7 +6,9 @@ const { Server } = require("socket.io")
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server)
+const io = new Server(server, {
+  connectionStateRecovery: {}
+})
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
